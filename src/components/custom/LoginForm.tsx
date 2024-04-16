dc8d387d09741967972ad90fb8852ce30049a387
Maaz <mdmaazi2907@gmail.com>
2024-04-16 16:15:10 +0530

    added login

--- src/components/custom/LoginForm.tsx
+++ src/components/custom/LoginForm.tsx
@@ -1,4 +1,5 @@
 import { authPageState } from "@/atoms/authPageAtom";
+import axios from "axios";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -9,13 +10,36 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useState } from "react";
 import { useSetRecoilState } from "recoil";
+
+interface LoginFormInput {
+  email: string;
+  password: string;
+}
 
 export function LoginForm() {
   const setAuthState = useSetRecoilState(authPageState);
-  
+  const [input, setInput] = useState<LoginFormInput>({
+    email: "",
+    password: "",
+  });
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  }
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const { data } = await axios.post(
+      "http://localhost:8000/api/v1/users/signin",
+      input,
+      { withCredentials: true }
+    );
+    console.log(data);
+  }
   return (
-    <form className="w-full max-w-sm m-auto">
+    <form className="w-full max-w-sm m-auto" onSubmit={handleSubmit}>
       <Card className="w-full max-w-sm m-auto">
         <CardHeader>
           <CardTitle className="text-2xl">Login</CardTitle>
@@ -28,11 +52,21 @@ export function LoginForm() {
               type="email"
               placeholder="m@example.com"
               required
+              name="email"
+              value={input.email}
+              onChange={handleChange}
             />
           </div>
           <div className="grid gap-2">
             <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" required />
+            <Input
+              id="password"
+              type="password"
+              required
+              name="password"
+              value={input.password}
+              onChange={handleChange}
+            />
           </div>
         </CardContent>
         <CardFooter>
@@ -43,7 +77,12 @@ export function LoginForm() {
         <CardFooter>
           don't have an account{" "}
           <span>
-            <Button onClick={()=> setAuthState('signup')} type="button" variant={"link"} className="w-full">
+            <Button
+              onClick={() => setAuthState("signup")}
+              type="button"
+              variant={"link"}
+              className="w-full"
+            >
               Sign up
             </Button>
           </span>
