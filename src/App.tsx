87bff1f7273fe86d958bbc71810e4ba7945bec9a
Maaz <mdmaazi2907@gmail.com>
2024-04-17 14:04:46 +0530

    signup done.

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,49 @@ import PostDetailsPage from "./pages/PostDetailsPage";
 import AuthPage from "./pages/AuthPage";
 import { useRecoilValue } from "recoil";
 import { userState } from "./atoms/userAtom";
+import ProtectedRoute from "./components/custom/ProtectedRoute";
 
 function App() {
   const user = useRecoilValue(userState);
   return (
     <Routes>
       <Route
-        path="/"
-        element={user ? <Navigate to={`/${user.username}`} /> : <AuthPage />}
-      />
-      <Route
         path="/auth"
         element={user ? <Navigate to={`/${user.username}`} /> : <AuthPage />}
       />
-      <Route path="/profile" element={<h1>Profile</h1>} />
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <h1>Home</h1>
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/profile"
+        element={
+          <ProtectedRoute>
+            <h1>Profile</h1>
+          </ProtectedRoute>
+        }
+      />
       <Route
         path="/:username"
-        element={user ? <UserHomePage /> : <Navigate to={"/auth"} />}
+        element={
+          <ProtectedRoute>
+            <UserHomePage />
+          </ProtectedRoute>
+        }
       />
-      <Route path="/:username/post/:id" element={<PostDetailsPage />} />
+      <Route
+        path="/:username/post/:id"
+        element={
+          <ProtectedRoute>
+            <PostDetailsPage />
+          </ProtectedRoute>
+        }
+      />
+      
     </Routes>
   );
 }
